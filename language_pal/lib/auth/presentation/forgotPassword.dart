4aecf643f892917e453a94b1d342af22baf547a6
Peer Rheinboldt <peer.rheinboldt@gmail.com>
2023-03-20 21:12:54 +0100

    Finished Auth UI and added better loading page

--- language_pal/lib/auth/presentation/forgotPassword.dart
+++ language_pal/lib/auth/presentation/forgotPassword.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:language_pal/auth/authProvider.dart';
-import 'package:language_pal/auth/presentation/signInPage.dart';
-import 'package:language_pal/auth/presentation/signUpPage.dart';
+import 'package:language_pal/auth/components/signInButton.dart';
 import 'package:provider/provider.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
-  Function(Widget) changeChild;
-  ForgotPasswordPage(this.changeChild, {Key? key}) : super(key: key);
+  const ForgotPasswordPage({Key? key}) : super(key: key);
 
   @override
   State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
@@ -14,7 +12,6 @@ class ForgotPasswordPage extends StatefulWidget {
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   final emailCont = TextEditingController();
-  final passCont = TextEditingController();
   String? errorMsg = "";
 
   void runAuth(Function() f) async {
@@ -33,13 +30,16 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Forgot Password"),
+      ),
       body: Consumer<AuthProvider>(
         builder: (context, ap, _) {
           return Stack(children: [
             Container(
               padding: const EdgeInsets.all(20),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   TextField(
                     keyboardType: TextInputType.emailAddress,
@@ -49,21 +49,18 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                       hintStyle: TextStyle(color: Colors.black54),
                     ),
                   ),
+                  const SizedBox(height: 5),
                   if (errorMsg != null)
                     Text(
                       errorMsg!,
                       style: const TextStyle(color: Colors.red),
                     ),
-                  ElevatedButton(
+                  const SizedBox(height: 5),
+                  CustomAuthButton(
                       onPressed: () {
                         runAuth(() => ap.forgotPassword(emailCont.text));
                       },
-                      child: const Text("Send Reset Email")),
-                  TextButton(
-                      onPressed: () {
-                        widget.changeChild(SignInPage(widget.changeChild));
-                      },
-                      child: const Text("Back to Sign In")),
+                      text: "Send Reset Email"),
                 ],
               ),
             ),
