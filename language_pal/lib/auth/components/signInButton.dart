f5fc87347546c5812f1409174539d21c36f5e4e5
Peer Rheinboldt <peer.rheinboldt@gmail.com>
2023-03-20 17:29:38 +0100

    added theme and started ui

---
+++ language_pal/lib/auth/components/signInButton.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomAuthButton extends StatelessWidget {
+  final String text;
+  final Function() onPressed;
+  const CustomAuthButton({required this.text, required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: ButtonStyle(
+          minimumSize: MaterialStateProperty.all(const Size.fromHeight(40))),
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
