ab34ff647cd10f0d5c51811a710914c1726db969
Peer Rheinboldt <peer.rheinboldt@gmail.com>
2023-03-28 18:32:18 +0200

    Fixed small detail. Getting ready for 1st Beta

--- language_pal/lib/app/user/presentation/user_page.dart
+++ language_pal/lib/app/user/presentation/user_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:language_pal/app/chat/models/messages.dart';
 import 'package:language_pal/app/scenario/scenarios_model.dart';
 import 'package:language_pal/app/user/logic/past_conversations.dart';
@@ -19,12 +18,13 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
-  List<Messages>? conversations = [];
+  List<Messages>? conversations;
   List<UseCaseModel> useCases = [];
   List<ScenarioModel> scenarios = [];
 
   Future<void> loadScenarios() async {
     AuthProvider ap = context.read();
+    if (ap.user == null) return;
     final tmp = await loadScenarioModels(
       ap.user!.learnLang,
       ap.user!.appLang,
@@ -39,6 +39,7 @@ class _UserPageState extends State<UserPage> {
   void loadConversations() async {
     if (conversations != null) return;
     AuthProvider ap = context.read<AuthProvider>();
+    if (ap.firebaseUser == null) return;
     var tmp = await loadPastConversations(scenarios, ap.firebaseUser!.uid);
     setState(() {
       conversations = tmp;
@@ -47,6 +48,7 @@ class _UserPageState extends State<UserPage> {
 
   void loadUseCases() async {
     AuthProvider ap = context.read<AuthProvider>();
+    if (ap.user == null) return;
     var tmp = await loadUseCaseModels(ap.user!.appLang);
     setState(() {
       useCases = tmp;
@@ -69,178 +71,199 @@ class _UserPageState extends State<UserPage> {
       appBar: AppBar(
         title: const Text("User"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Icon(Icons.account_circle_outlined, size: 54, fill: 0.0),
-                const SizedBox(width: 16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                        ap.firebaseUser!.displayName ?? ap.firebaseUser!.email!,
-                        style: Theme.of(context).textTheme.headlineSmall),
-                    const SizedBox(height: 4),
-                    if (ap.firebaseUser!.displayName != null)
-                      Text(ap.firebaseUser!.email!),
-                  ],
-                ),
-                const Expanded(
-                  child: SizedBox(),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.logout),
-                  onPressed: () {
-                    ap.signOut();
-                  },
-                )
-              ],
-            ),
-            const SizedBox(height: 16),
-            const Divider(),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                const Icon(Icons.language, size: 54),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: Table(
+      body: ap.user == null
+          ? const Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      TableRow(children: [
-                        TableCell(
-                          verticalAlignment: TableCellVerticalAlignment.middle,
-                          child: Text(
-                              AppLocalizations.of(context)!.user_page_app_lang,
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.w600)),
-                        ),
-                        DropdownButton(
-                          value: ap.user!.appLang,
-                          items: supportedAppLanguages().map((e) {
-                            return DropdownMenuItem(
-                                value: e.code,
-                                child: Text("${e.emoji}${e.getName(context)}"));
-                          }).toList(),
-                          onChanged: (e) {
-                            UserModel newUser = ap.user!;
-                            newUser.appLang = e!;
-                            ap.setUserModel(newUser);
-                          },
-                        )
-                      ]),
-                      TableRow(children: [
-                        TableCell(
-                          verticalAlignment: TableCellVerticalAlignment.middle,
-                          child: Text(
-                              AppLocalizations.of(context)!
-                                  .user_page_learn_lang,
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.w600)),
-                        ),
-                        DropdownButton(
-                          value: ap.user!.learnLang,
-                          items: supportedLearnLanguages().map((e) {
-                            return DropdownMenuItem(
-                                value: e.code,
-                                child: Text("${e.emoji}${e.getName(context)}"));
-                          }).toList(),
-                          onChanged: (e) {
-                            UserModel newUser = ap.user!;
-                            newUser.learnLang = e!;
-                            ap.setUserModel(newUser);
-                          },
-                        )
-                      ]),
-                      TableRow(children: [
-                        TableCell(
-                          verticalAlignment: TableCellVerticalAlignment.middle,
-                          child: Text(
-                              AppLocalizations.of(context)!.user_page_use_case,
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.w600)),
-                        ),
-                        DropdownButton(
-                          isExpanded: true,
-                          value: ap.user!.useCase,
-                          items: useCases
-                              .map((e) => DropdownMenuItem(
-                                  value: e.uniqueId,
-                                  child: Text(
-                                    e.emoji + e.title,
-                                    softWrap: false,
-                                    maxLines: 1,
-                                    overflow: TextOverflow.fade,
-                                  )))
-                              .toList(),
-                          onChanged: (e) {
-                            UserModel newUser = ap.user!;
-                            newUser.useCase = e!;
-                            ap.setUserModel(newUser);
-                          },
-                        )
-                      ]),
+                      const Icon(Icons.account_circle_outlined,
+                          size: 54, fill: 0.0),
+                      const SizedBox(width: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              ap.firebaseUser!.displayName ??
+                                  ap.firebaseUser!.email!,
+                              style: Theme.of(context).textTheme.headlineSmall),
+                          const SizedBox(height: 4),
+                          if (ap.firebaseUser!.displayName != null)
+                            Text(ap.firebaseUser!.email!),
+                        ],
+                      ),
+                      const Expanded(
+                        child: SizedBox(),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.logout),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          ap.signOut();
+                        },
+                      )
                     ],
                   ),
-                )
-              ],
-            ),
-            const SizedBox(height: 16),
-            const Divider(),
-            const SizedBox(height: 16),
-            Text(AppLocalizations.of(context)!.user_page_conversations_title,
-                style: Theme.of(context).textTheme.headlineSmall),
-            if (conversations == null)
-              const Center(child: CircularProgressIndicator())
-            else if (conversations!.isEmpty)
-              Text(AppLocalizations.of(context)!.user_page_no_conversations),
-            Expanded(
-                child: ListView.builder(
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                PastConversationPage(conversations![index])));
-                  },
-                  child: Card(
-                    child: ListTile(
-                      leading: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.min,
+                  const SizedBox(height: 16),
+                  const Divider(),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      const Icon(Icons.language, size: 54),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Table(
                           children: [
-                            Text(conversations![index].scenario.emoji),
-                            const SizedBox(width: 12),
-                            SizedBox(
-                              width: 14,
-                              height: 14,
-                              child: CircularProgressIndicator(
-                                value:
-                                    (conversations![index].rating?.score ?? 0) /
-                                        100,
-                                strokeWidth: 3,
-                                valueColor: AlwaysStoppedAnimation<Color>(
-                                    Theme.of(context).colorScheme.primary),
+                            TableRow(children: [
+                              TableCell(
+                                verticalAlignment:
+                                    TableCellVerticalAlignment.middle,
+                                child: Text(
+                                    AppLocalizations.of(context)!
+                                        .user_page_app_lang,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600)),
                               ),
-                            ),
-                          ]),
-                      title: Text(conversations![index].scenario.name),
-                      trailing: const Icon(Icons.chevron_right),
-                    ),
+                              DropdownButton(
+                                value: ap.user!.appLang,
+                                items: supportedAppLanguages().map((e) {
+                                  return DropdownMenuItem(
+                                      value: e.code,
+                                      child: Text(
+                                          "${e.emoji}${e.getName(context)}"));
+                                }).toList(),
+                                onChanged: (e) {
+                                  UserModel newUser = ap.user!;
+                                  newUser.appLang = e!;
+                                  ap.setUserModel(newUser);
+                                },
+                              )
+                            ]),
+                            TableRow(children: [
+                              TableCell(
+                                verticalAlignment:
+                                    TableCellVerticalAlignment.middle,
+                                child: Text(
+                                    AppLocalizations.of(context)!
+                                        .user_page_learn_lang,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600)),
+                              ),
+                              DropdownButton(
+                                value: ap.user!.learnLang,
+                                items: supportedLearnLanguages().map((e) {
+                                  return DropdownMenuItem(
+                                      value: e.code,
+                                      child: Text(
+                                          "${e.emoji}${e.getName(context)}"));
+                                }).toList(),
+                                onChanged: (e) {
+                                  UserModel newUser = ap.user!;
+                                  newUser.learnLang = e!;
+                                  ap.setUserModel(newUser);
+                                },
+                              )
+                            ]),
+                            TableRow(children: [
+                              TableCell(
+                                verticalAlignment:
+                                    TableCellVerticalAlignment.middle,
+                                child: Text(
+                                    AppLocalizations.of(context)!
+                                        .user_page_use_case,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600)),
+                              ),
+                              DropdownButton(
+                                isExpanded: true,
+                                value: ap.user!.useCase,
+                                items: useCases
+                                    .map((e) => DropdownMenuItem(
+                                        value: e.uniqueId,
+                                        child: Text(
+                                          e.emoji + e.title,
+                                          softWrap: false,
+                                          maxLines: 1,
+                                          overflow: TextOverflow.fade,
+                                        )))
+                                    .toList(),
+                                onChanged: (e) {
+                                  UserModel newUser = ap.user!;
+                                  newUser.useCase = e!;
+                                  ap.setUserModel(newUser);
+                                },
+                              )
+                            ]),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                );
-              },
-              itemCount: conversations!.length,
-            ))
-          ],
-        ),
-      ),
+                  const SizedBox(height: 16),
+                  const Divider(),
+                  const SizedBox(height: 16),
+                  Text(
+                      AppLocalizations.of(context)!
+                          .user_page_conversations_title,
+                      style: Theme.of(context).textTheme.headlineSmall),
+                  if (conversations == null)
+                    const CircularProgressIndicator()
+                  else if (conversations!.isEmpty)
+                    Text(AppLocalizations.of(context)!
+                        .user_page_no_conversations)
+                  else
+                    Expanded(
+                        child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => PastConversationPage(
+                                        conversations![index])));
+                          },
+                          child: Card(
+                            child: ListTile(
+                              leading: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(conversations![index].scenario.emoji),
+                                    const SizedBox(width: 12),
+                                    SizedBox(
+                                      width: 14,
+                                      height: 14,
+                                      child: CircularProgressIndicator(
+                                        value: (conversations![index]
+                                                    .rating
+                                                    ?.score ??
+                                                0) /
+                                            100,
+                                        strokeWidth: 3,
+                                        valueColor:
+                                            AlwaysStoppedAnimation<Color>(
+                                                Theme.of(context)
+                                                    .colorScheme
+                                                    .primary),
+                                      ),
+                                    ),
+                                  ]),
+                              title: Text(conversations![index].scenario.name),
+                              trailing: const Icon(Icons.chevron_right),
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount: conversations!.length,
+                    )),
+                ],
+              ),
+            ),
     );
   }
 }
